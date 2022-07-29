fc2a80d5d417b5e59a998b7ae990a42f50cd5aea
ivannikovlg13 <livannikov55@gmail.com>
2022-07-30 00:49:35 +0300

    working with filters

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json').then((response) =>
+      response.json().then((json) => setPizzas(json.pizzas)),
+    );
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home items={pizzas} />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
